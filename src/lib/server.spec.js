222827de11d6033e17a3e182305f289151818004
AJ Hsu <ajhsu@kkbox.com>
2017-03-09 23:46:18 +0800

    Add test case for favicon.ico requests

--- src/lib/server.spec.js
+++ src/lib/server.spec.js
@@ -8,18 +8,40 @@ test('Basic server operation', async t => {
   // arrange
   const server = new Server();
   server.boot();
+
   // act
-  const response = await requestPromise({
+  const healthResponse = await requestPromise({
     uri: 'http://127.0.0.1:3002/health',
     resolveWithFullResponse: true,
     json: true
   });
-  t.equal(response.statusCode, 200, '/health should return 200');
+  t.equal(
+    healthResponse.statusCode,
+    200,
+    '/health should return 200'
+  );
   t.deepEqual(
-    response.body,
+    healthResponse.body,
     { status: 'good' },
     '/health should return {status: good} in json format'
   );
+
+  // act
+  const faviconResponse = await requestPromise({
+    uri: 'http://127.0.0.1:3002/favicon.ico',
+    resolveWithFullResponse: true
+  });
+  t.equal(
+    faviconResponse.statusCode,
+    200,
+    '/favicon.ico should return 200'
+  );
+  t.deepEqual(
+    faviconResponse.body,
+    '',
+    '/favicon.ico should return nothing but empty string'
+  );
+  
   // teardown
   server.shutdown();
   t.end();
