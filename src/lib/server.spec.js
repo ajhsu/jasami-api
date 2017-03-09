e6c56bccb1e9622e17252ffe0a8da8ad702da630
AJ Hsu <ajhsu@kkbox.com>
2017-03-09 23:18:05 +0800

    Add basic server test

--- src/lib/server.spec.js
+++ src/lib/server.spec.js
@@ -1,7 +1,22 @@
+require('babel-polyfill');
+
 import test from 'tape';
 import Server from './server';
+import requestPromise from 'request-promise';
 
-test('Basic server operation', t => {
+test('Basic server operation', async t => {
   t.plan(1);
-  t.ok(1 == 1, 'one equals to one');
+  // arrange
+  const s = new Server();
+  s.boot();
+  // assert
+  const expected = { status: 'good' };
+  // act
+  const response = await requestPromise({
+    uri: 'http://127.0.0.1:3002/health',
+    json: true
+  });
+  t.deepEqual(response, expected, '/health should be good');
+  // teardown
+  s.shutdown();
 });
