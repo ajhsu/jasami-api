f0bf2f9d43a29de5173929ea583c1a4685f06ec6
AJ Hsu <ajhsu@kkbox.com>
2017-03-10 16:27:51 +0800

    Add new endpoints

--- src/lib/middlewares/jasami.js
+++ src/lib/middlewares/jasami.js
@@ -252,6 +252,38 @@ const fakeDatabase = [
   }
 ];
 
-export const getRestaurants = (req, res, next) => {
-  res.status(200).json(fakeDatabase);
+// GET /restaurants
+export const getAllRestaurants = (req, res, next) => {
+  const restaurants = fakeDatabase;
+  res.status(200).json(restaurants);
 };
+// GET /restaurant
+export const getRestaurtantById = (req, res, next) => {
+  const restaurantId = req.params.restaurantId;
+  res.end('ok');
+};
+// POST /restaurant
+export const addRestaurtant = (req, res, next) => {
+  console.log(req.payload);
+  res.json({ status: 'success' });
+};
+// PUT /restaurant
+export const updateRestaurtantById = (restaurantId, payload) => {};
+
+// GET /restaurant/<id>/dishes
+export const getAllDishesByRestaurantId = restaurantId => {};
+// GET /restaurant/<id>/dish/<id>
+export const getDishByRestaurantIdAndDishId = (restaurantId, dishId) => {};
+// POST /restaurant/<id>/dish
+export const addDishByRestaurantId = (restaurantId, payload) => {};
+// PUT /restaurant/<id>/dish/<id>
+export const updateDishByRestaurantIdAndDishId = (
+  restaurantId,
+  dishId,
+  payload
+) => {};
+
+// GET /search?keyword=<text>
+export const search = keyword => {};
+// GET /search?tags=<tag1>,<tag2>
+export const filterByTags = tags => {};
--- src/lib/middlewares/routes.js
+++ src/lib/middlewares/routes.js
@@ -1,12 +1,18 @@
 import { Router } from 'express';
 import healthCheck from './health-check';
 import favicon from './favicon';
-import { getRestaurants } from './jasami';
+import {
+  getAllRestaurants,
+  getRestaurtantById,
+  addRestaurtant
+} from './jasami';
 
 const router = Router();
 
 router.route('/health').get(healthCheck);
 router.route('/favicon.ico').get(favicon);
-router.route('/restaurants').get(getRestaurants);
+router.route('/restaurants').get(getAllRestaurants);
+router.route('/restaurant/:restaurantId').get(getRestaurtantById);
+router.route('/restaurant').post(addRestaurtant);
 
 export default router;
